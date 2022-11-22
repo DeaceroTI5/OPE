c17f3f268c420f509f2f31a3bb6e1ad714a5b525
hvalle145 <85528900+hvalle145@users.noreply.github.com>
2022-11-22 12:09:18 -0600

    COMMIT 550.25

--- Procedimientos/OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel.sql
+++ Procedimientos/OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel.sql
@@ -2,8 +2,10 @@ GO
 -- 'OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel'
 GO
 ALTER PROCEDURE OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel
-@pnClaUbicacion INT,
-@psEstatusEntrega VARCHAR(20)
+@pnClaUbicacion		INT,
+@psEstatusEntrega	VARCHAR(20),
+@pnKilos			NUMERIC(22, 4),
+@pnClaArticulo		INT
 AS
 BEGIN
 
@@ -15,6 +17,17 @@ BEGIN
 		RETURN
 		
 	END
+
+	
+	DECLARE @nPesoTeoricoKgs INT
+	
+	SELECT	@nPesoTeoricoKgs = PesoTeoricoKgs 
+	FROM	OpeSch.OpeArtCatArticuloVw WITH(NOLOCK) 
+	WHERE	ClaArticulo = @pnClaArticulo 
+	AND		ClaTipoInventario = 1
+	
+	IF ISNULL(@nPesoTeoricoKgs,0) > 0
+		SELECT Cantidad = @pnKilos / @nPesoTeoricoKgs
 
 
 END
