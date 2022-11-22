6b74cfee09f5c2565ca14362346f725e0c65eed2
hvalle145 <85528900+hvalle145@users.noreply.github.com>
2022-11-22 12:08:44 -0600

    commit 550.25

---
+++ Procedimientos/OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel.sql
@@ -0,0 +1,20 @@
+GO
+-- 'OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel'
+GO
+ALTER PROCEDURE OPESch.OPE_CU550_Pag25_Grid_ViajesPorRecibirDet_CambioValor_Kilos_Sel
+@pnClaUbicacion INT,
+@psEstatusEntrega VARCHAR(20)
+AS
+BEGIN
+
+	IF @psEstatusEntrega = 'No Entregado'
+	BEGIN
+		
+		RAISERROR('No se puede modificar una factura que no trae evidencia. Favor de revisar.',16, 1)
+		SELECT Kilos = 0
+		RETURN
+		
+	END
+
+
+END
